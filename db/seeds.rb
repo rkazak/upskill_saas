9baac6f907a02276e4ddbfd3d7c8c31b1e0d7b9e
Rohinton Kazak <Rohinton.Kazak@gmail.com>
2017-09-29 16:52:33 +0000

    Added association between users and plans.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Plan.create(name: 'basic', price: 0)
-Plan.create(name: 'pro', price: 100)
+Plan.create(name: 'pro', price: 10)
