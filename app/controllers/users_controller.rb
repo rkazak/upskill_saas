6831f9c99f048abc6eb544ff856fcbed157d41b3
Rohinton Kazak <Rohinton.Kazak@gmail.com>
2017-10-06 21:48:30 +0000

    Added users index action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   
-  # GET to /users/:id
+  def index
+  end
   
+  # GET to /users/:id
   def show
     @user = User.find( params[:id] )
   end
