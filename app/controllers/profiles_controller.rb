cb757474cc46a24b649540dc9e1ae83536e8a54b
Rohinton Kazak <Rohinton.Kazak@gmail.com>
2017-10-06 21:39:54 +0000

    Added before filters to ensure correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
   
   # GET /users/:user_id/profile/new
   def new
@@ -48,6 +50,11 @@ class ProfilesController < ApplicationController
   def profile_params
     # strong assignment, white-listing what's allowed to pass through the form from the user.
     params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+  end
+  
+  def only_current_user
+    @user = User.find( params[:user_id])
+    redirect_to(root_url) unless @user == current_user
   end
   
 end
